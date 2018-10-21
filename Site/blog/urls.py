996eb3db4e551a3be680b3509073a2c21e7bafc9
rakibul1 <mrakibul70@gmail.com>
2018-10-21 19:12:13 +0600

    Working

--- Site/blog/urls.py
+++ Site/blog/urls.py
@@ -1,7 +1,14 @@
 from django import views
 from django.urls import path
+from django.conf.urls import url
 from . import views
+
 urlpatterns = [
     path('', views.pools, name='blog'),
     path('', views.home_view, name='home_view'),
+    url('', views.index, name='index'),
+    url(r'^post/$',views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$',views.post_details, name='post_details'),
+    url(r'^post/(?P<pk>[0-9]+)/comment/$',views.post_comment, name='post_comment'),
+    url(r'^post/(?P<pk>[0-9]+)/comment/add_comment/$',views.add_comment, name='add_comment'),
 ]
--- Site/blog/views.py
+++ Site/blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404, redirect
+from .models import Post, Comment
+from datetime import datetime
+#from .forms import CommentForm
+from django.http import HttpResponse, Http404
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
+
 
 def pools(request):
     return HttpResponse("Hello")
@@ -10,3 +15,28 @@ def pools(request):
 
 def home_view(request):
     return HttpResponse("hi")
+
+
+def index(request):
+    try:
+        posts = Post.objects.all()
+    except ObjectDoesNotExist:
+        raise Http404("Post does not exit!")
+
+    return HttpResponse("Work")
+
+
+def post_list():
+    pass
+
+
+def post_details():
+    pass
+
+
+def post_comment():
+    pass
+
+
+def add_comment():
+    pass
